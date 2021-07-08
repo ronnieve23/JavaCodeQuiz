6ef81f3dc9634c657f3bbdbc8f23096231503d06
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-07 20:55:23 -0400

    Initiated HTML and JS with code provided by instructor

---
+++ assets/js/quiz.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts",
+    },
+    {
+      question:
+        "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses",
+    },
+  ];
