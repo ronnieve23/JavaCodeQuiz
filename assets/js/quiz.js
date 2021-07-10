d0f7b395898a9c7d12f09871d77cc3f2bd5d0bbe
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-10 14:07:27 -0400

    Added JS code given by instructor

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,13 +1,38 @@
 var questions = [
-    {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts",
-    },
-    {
-      question:
-        "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses",
-    },
-  ];
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+];
+
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
+
+var questionIndex = 0;
+var correctCount = 0;
+
+function renderQuestion() {
+  questionEl.textContent = questions[questionIndex].question;
+
+  optionListEl.innerHTML = "";
+
+  var choices = questions[questionIndex].choices;
+  var choicesLength = choices.length;
+
+  for (var i = 0; i < choicesLength; i++) {
+    var questionListItem = document.createElement("li");
+    questionListItem.textContent = choices[i];
+    optionListEl.append(questionListItem);
+  }
+}
+
+renderQuestion();
